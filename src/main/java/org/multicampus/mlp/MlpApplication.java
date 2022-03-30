a9c8220f6976fa6042eecd9b891f8def2c236249
choi-yunji <dbswl5883@naver.com>
2022-03-30 21:07:00 +0900

    Initial commit

---
+++ src/main/java/org/multicampus/mlp/MlpApplication.java
@@ -0,0 +1,13 @@
+package org.multicampus.mlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/multicampus/mlp/MlpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.multicampus.mlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
